f13ff646b0ea7d5d5ce5442681b95cfcd54bcdcc
Johannes Förstner <johannes@famfoerstner.de>
2026-01-18 16:12:03 +0100

    Clean up demo map as well

--- cleanup.bat
+++ cleanup.bat
@@ -1,8 +1,11 @@
 :: name of mod
 set mod=Wintermod
+:: name of demo map
+set map=wintermod-demo
 
 :: path of DS installation
 set ds=%DungeonSiege%
 
 :: Cleanup resources so as not to confuse Siege Editor
 del "%ds%\DSLOA\%mod%.dsres"
+del "%ds%\DSLOA\%map%.dsmap"

abdc7e8082427658ad62cb6ba413eb34aa2a3167
Johannes Förstner <johannes@famfoerstner.de>
2026-01-19 19:31:45 +0100

    Add generate-moods.bat

---
+++ generate-moods.bat
@@ -0,0 +1,7 @@
+:: path of Bits dir
+set bits=%~dp0.
+
+pushd %gaspy%
+venv\Scripts\python -m landscaping.edit_moods --src world/global/git-ignore/moods --edit-from-file mood-edits.txt --bits "%bits%"
+if %errorlevel% neq 0 pause
+popd
